edfb1907a3cebcee7e13dcd82c071faa5a0b4d33
jasconm <50549004+jasconm@users.noreply.github.com>
2019-05-12 17:55:18 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+---
+layout: null
+sitemap:
+  exclude: 'yes'
+---
+
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
@@ -7,26 +13,17 @@ $(document).ready(function () {
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
     }
   })
-
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
+  if (window.location.hash && window.location.hash == '#blog') { $('.panel-cover').addClass('panel-cover--collapsed') }
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}/index.html') { $('.panel-cover').addClass('panel-cover--collapsed') }
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
-
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
-
 })
